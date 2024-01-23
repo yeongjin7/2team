2de076c4b80c4533aad5220f2f44074ae0d05166
wastyno100 <wastyno100@gmail.com>
2024-01-23 17:57:56 +0900

    1

--- project02/travel01/server/router/controllers/admin.js
+++ project02/travel01/server/router/controllers/admin.js
@@ -16,6 +16,55 @@ const saveSqAnswer = async (req, res) => {
     }
 }
 
+const getUserList = async (req, res) => {
+    try {
+        var conn = await db.getConnection(); // db에 연결
+        const raws = await conn.query("SELECT * FROM user"); // 쿼리문 실행
+
+        if(raws){
+          res.send(raws); // res => 응답으로, send => 보냅니다. (vue에서 보이게)
+      }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const userDelete = async (req, res) => {
+    try {
+        var conn = await db.getConnection();
+        const userId = req.params.userId;
+        const query = "UPDATE user SET name = '탈퇴', id = '탈퇴', password = '탈퇴', email = '탈퇴', phone = '탈퇴' WHERE userNo = ?;";
+        const result = conn.query(query, userId);
+        if(result){
+            res.send({ success: true });
+        }
+
+    } catch (error) {
+        console.error(err);
+    }
+}
+
+const getqnalistadmin = async (req, res) => {
+    try {
+        var conn = await db.getConnection();
+        const raws = await conn.query("SELECT singleqna.*, user.id FROM singleqna JOIN user ON singleqna.userNo = user.userNo");
+        console.log('어드민 문의', raws)
+        raws.forEach(item => {
+            const stringDate = item.sqDate;
+            const dateObject = new Date(stringDate);
+            const formattedDate = `${dateObject.getFullYear()}-${(dateObject.getMonth() + 1).toString().padStart(2, '0')}-${dateObject.getDate().toString().padStart(2, '0')} ${dateObject.getHours().toString().padStart(2, '0')}:${dateObject.getMinutes().toString().padStart(2, '0')}`;
+            item.sqDate = formattedDate;
+            console.log("2", formattedDate);
+        });
+        res.send(raws);
+    } catch (err) {
+        console.err(err);
+    }
+}
+
 module.exports = {
     saveSqAnswer,
+    getUserList,
+    userDelete,
+    getqnalistadmin,
 }
